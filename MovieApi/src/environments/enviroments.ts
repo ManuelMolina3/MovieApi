2cda5b9bacae8a8bb739b255ddf8f7aff28cdc7c
Manolo <mmgmolina03@gmail.com>
2023-11-15 20:15:23 +0100

    comienzo de la autentificacion

--- MovieApi/src/environments/enviroments.ts
+++ MovieApi/src/environments/enviroments.ts
@@ -1,18 +1,13 @@
 export const environment = {
 
-    apiKey: 'c1145bae31c48bd921e13ad767b7fb3a',
-    
+    apiKey: 'api_key=c1145bae31c48bd921e13ad767b7fb3a',
+    baseUrlSeries: 'https://api.themoviedb.org/3/tv/popular',
     baseUrlSeriesDetails: 'https://api.themoviedb.org/3/tv/',
     baseUrlMovie: 'https://api.themoviedb.org/3/movie/popular',
     baseUrlMovieDetails: 'https://api.themoviedb.org/3/movie/',
-
-
-    
-    baseUrlSeries: 'https://api.themoviedb.org/3'
-
-
-
     baseUrlPeople: 'https://api.themoviedb.org/3/person/popular',
-    baseUrlPeopleDetails: 'https://api.themoviedb.org/3/person/'
+    baseUrlPeopleDetails: 'https://api.themoviedb.org/3/person/',
+    baseUrl: 'https://api.themoviedb.org/3',
+    tmbdtoken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMTE0NWJhZTMxYzQ4YmQ5MjFlMTNhZDc2N2I3ZmIzYSIsInN1YiI6IjY1NDBjNWI3NDU1N2EwMDBhZDEyMjA5ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.x78Kq6blQQYovk11Br3WEeuRk0_fd5QCuCvMoobF4To'
 
 };
